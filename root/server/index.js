441bcfdb4894137804365af10df87645e6b138a2
alejandro <vamg008@gmail.com>
2013-02-26 14:25:27 -0600

    [fix] missing closing quote

--- root/server/index.js
+++ root/server/index.js
@@ -1,7 +1,7 @@
 var http = require('http')
   , ecstatic = require('ecstatic')
 
-http.createServer(ecstatic(__dirname + '/../app/, {
+http.createServer(ecstatic(__dirname + '/../app/', {
     autoIndex: true
   , defaultExt: 'html'
 })).listen(process.env.PORT || 8080, function (){
